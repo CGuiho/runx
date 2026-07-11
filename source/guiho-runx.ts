966484556192b20dc9ef04b0d504099b4ee25519
Cristóvão GUIHO <cristovaoguiho@gmail.com>
2026-07-12 00:46:50 +0200

    Add RunX library exports

---
+++ source/guiho-runx.ts
@@ -0,0 +1,3 @@
+export { runCli, runCliWithErrorHandling } from './cli.js'
+export { findManifest, readManifest, resolveCommand } from './manifest.js'
+export type { CliOptions, ResolvedCommand, RunXCommand, RunXManifest } from './types.js'
