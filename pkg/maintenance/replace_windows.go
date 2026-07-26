510a9aa76923bd7767644666343cb8633ce6626e
Cristóvão GUIHO <cristovaoguiho@gmail.com>
2026-07-26 02:09:19 +0200

    Add pkg/maintenance/replace_windows.go

---
+++ pkg/maintenance/replace_windows.go
@@ -0,0 +1,27 @@
+//go:build windows
+
+package maintenance
+
+import (
+	"fmt"
+	"syscall"
+	"unsafe"
+)
+
+var moveFileEx = syscall.NewLazyDLL("kernel32.dll").NewProc("MoveFileExW")
+
+func replaceFileAtomic(source, destination string) error {
+	from, err := syscall.UTF16PtrFromString(source)
+	if err != nil {
+		return err
+	}
+	to, err := syscall.UTF16PtrFromString(destination)
+	if err != nil {
+		return err
+	}
+	result, _, callErr := moveFileEx.Call(uintptr(unsafe.Pointer(from)), uintptr(unsafe.Pointer(to)), uintptr(0x1|0x8))
+	if result == 0 {
+		return fmt.Errorf("MoveFileExW: %w", callErr)
+	}
+	return nil
+}
