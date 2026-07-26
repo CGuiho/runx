55499ee09581f5df74602fb8b012c43857a0372e
Cristóvão GUIHO <cristovaoguiho@gmail.com>
2026-07-26 02:09:09 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"runtime/debug"
+
 	"github.com/CGuiho/runx/cmd"
 )
 
+var (
+	version     = "dev"
+	commit      = "unknown"
+	buildDate   = "unknown"
+	buildTarget = "development"
+)
+
 func main() {
-	cmd.Execute()
+	if version == "dev" {
+		if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "" && info.Main.Version != "(devel)" {
+			version = info.Main.Version
+		}
+	}
+	err := cmd.Execute(cmd.BuildInfo{Version: version, Commit: commit, Date: buildDate, Target: buildTarget})
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(cmd.ExitCode(err))
+	}
 }
